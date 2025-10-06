1ee3b9542c3874ba28d5716f64200ce6782ac16a
Lautaro-Espinosa015 <espinosalautii@outlook.com>
2025-10-06 19:54:31 -0300

    actualizacion

--- Assets/Prefabs/Player/patterns/MovimientoLateral.cs
+++ Assets/Prefabs/Player/patterns/MovimientoLateral.cs
@@ -6,7 +6,7 @@ public class MovimientoLateral : iMovementStrategy
         public void Move(Transform transform, Player player, float direccion)
         {
             //float direccion = Input.GetAxis("Horizontal"); // Obtiene la dirección horizontal del input
-        float moveInX = direccion * player.Velocity * Time.deltaTime;
+        float moveInX = direccion * player.Velocity * Time.deltaTime; // Calcula el movimiento lateral basado en la dirección y la velocidad
         transform.Translate(moveInX,0,0); // Mueve el jugador lateralmente
         }
     
